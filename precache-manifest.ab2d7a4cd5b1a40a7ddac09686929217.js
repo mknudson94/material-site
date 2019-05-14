44d34282a3668f343b4491d01e322b64f33ca2dc
Michael Knudson <m.knudso@gmail.com>
2019-05-14 11:31:14 -0400

    Updates

---
+++ precache-manifest.ab2d7a4cd5b1a40a7ddac09686929217.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506adbbf426ab68bd28261e85589d099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c03e739b21cb364907e",
+    "url": "/static/js/2.466d10e6.chunk.js"
+  },
+  {
+    "revision": "fc82e9cd2ee27a0351b2",
+    "url": "/static/js/main.00b73b21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a43b6f69898593842d47ee27c5b8a886",
+    "url": "/static/media/formal_xs.a43b6f69.jpg"
+  },
+  {
+    "revision": "5cfaa319393e17d2c118113bc9dac425",
+    "url": "/static/media/head-shot_small.5cfaa319.jpg"
+  },
+  {
+    "revision": "dc5f73a43fa79626ca00bf47f02265a8",
+    "url": "/static/media/material-logo.dc5f73a4.svg"
+  },
+  {
+    "revision": "e60feac2d41c02f67239d7504a36567c",
+    "url": "/static/media/react-logo.e60feac2.jpg"
+  },
+  {
+    "revision": "6b29cac4f691a8fd573be1bc2f97fcbb",
+    "url": "/static/media/rookjump.6b29cac4.png"
+  }
+]);
--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default ({
-    serviceID: 'default_service',
-    templateID: 'mytemplate',
-    userID: 'user_Ly2PMsfxkyN9Y0nalmUPk',
-});
